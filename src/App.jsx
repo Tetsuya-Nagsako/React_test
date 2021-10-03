584902ed4462280f7d58e62b8deea88d3eae4e13
Tetsuya-Nagsako <76085885+Tetsuya-Nagsako@users.noreply.github.com>
2021-10-03 21:30:18 +0900

    React-trial

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => {
-    alert();
+  const [num, setNum] = useState(0);
+  const [face, setFace] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
   };
+
+  const onClickFaceChane = () => {
+    setFace(!face);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        face || setFace(true);
+      } else {
+        face && setFace(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
 
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfulMessage color="blue">お元気ですか</ColorfulMessage>
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickFaceChane}>on/off</button>
+      <p>{num}</p>
+      {face && <p>^^</p>}
     </>
   );
 };
